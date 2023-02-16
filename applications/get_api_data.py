0ca383f26f3ba1a49731fc14549969de1db2c24d
Hugo Urbina G <103804882+hugoReborn@users.noreply.github.com>
2023-02-16 15:59:36 -0300

    getting data commit

---
+++ applications/get_api_data.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+URL = 'http://api.citybik.es/v2/networks/bikesantiago'
+
+
+def get_api_data(url):
+    try:
+        response = requests.get(url)
+        content = json.loads(response.content)
+
+        return content
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+hola = get_api_data(URL)
+sala = hola['network']['stations']
+
+for i in sala:
+    print(i['extra']['altitude'])
+  # print(i['extra']['address']) accedemos a la direccion dentro de stations y extra
+  # print(i['empty_slots']) accedemos directamente
+  # print(i['timestamp']) imprime la fecha y la hora dentro del diccionario en stations
+  # print(i['name']) imprime el nombre de las estaciones dentro del diccionario en stations
